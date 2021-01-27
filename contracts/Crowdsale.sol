2a63f1fbb1de483be9a0e4c684dc351e8bddb05d
Akshit Choudhary <Akkii4>
2021-01-27 20:19:25 +0530

    Vesting Added

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -136,8 +136,67 @@ library TransferHelper {
 
 }
 
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
 
-contract Crowdsale  {
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor () internal {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
+
+contract Crowdsale is ReentrancyGuard  {
     using SafeMath for uint256;
 
     address public owner;
@@ -147,7 +206,6 @@ contract Crowdsale  {
     IERC20 public token;
     address public Launchpad;
     
-    mapping(address => uint256) public claimable;
     IERC20 private usdc = IERC20(0xb7a4F3E9097C08dA09517b5aB877F7a917224ede);
     IERC20 private dai = IERC20(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa);
     IERC20 private usdt = IERC20(0x07de306FF27a2B630B1141956844eB1552B956B5);
@@ -165,6 +223,30 @@ contract Crowdsale  {
         uint256 endTime;
         uint256 cliffDurationInSecs;
     }
+    
+    /// @notice event emitted when a vesting schedule is created
+    event ScheduleCreated(address indexed _beneficiary);
+
+    /// @notice event emitted when a successful drawn down of vesting tokens is made
+    event DrawDown(address indexed _beneficiary, uint256 indexed _amount);
+
+    /// @notice start of vesting period as a timestamp
+    uint256 public start;
+
+    /// @notice end of vesting period as a timestamp
+    uint256 public end;
+
+    /// @notice cliff duration in seconds
+    uint256 public cliffDuration;
+
+    /// @notice amount vested for a beneficiary. Note beneficiary address can not be reused
+    mapping(address => uint256) public vestedAmount;
+
+    /// @notice cumulative total of tokens drawn down (and transferred from the deposit account) per beneficiary
+    mapping(address => uint256) public totalDrawn;
+
+    /// @notice last drawn down time (seconds) per beneficiary
+    mapping(address => uint256) public lastDrawnAt;
     
     CrowdsaleInfo public crowdsaleInfo;
 
@@ -203,24 +285,34 @@ contract Crowdsale  {
         external
     {
         require(crowdsaleOver == false, "Crowdsale has ended,invest in other crowdsale");
-
+        
+        uint256 vestingAmount;
+        
         if (_stableCoin == usdt) {
-            claimable[msg.sender] = claimable[msg.sender].add(
-                amount.mul(1e12).mul(rate).div(1e18)
-            );
+            vestingAmount = amount.mul(1e12).mul(rate).div(1e18);
             doTransferIn(address(_stableCoin), msg.sender, amount);
         } else if (_stableCoin == usdc) {
-            claimable[msg.sender] = claimable[msg.sender].add(
-                amount.mul(1e12).mul(rate).div(1e18)
-            );
+            vestingAmount = amount.mul(1e12).mul(rate).div(1e18);
             _stableCoin.transferFrom(msg.sender, address(this), amount);
         } else if (_stableCoin == dai) {
-            
-            claimable[msg.sender] = claimable[msg.sender].add(
-                amount.mul(rate).div(1e18)
-            );
+            vestingAmount = amount.mul(rate).div(1e18);
             _stableCoin.transferFrom(msg.sender, address(this), amount);
         }
+        return _createVestingSchedule(msg.sender, vestingAmount);
+    }
+    
+    function _createVestingSchedule(address _beneficiary, uint256 _amount) internal returns (bool) {
+        require(_beneficiary != address(0), "Beneficiary cannot be empty");
+        require(_amount > 0, "Amount cannot be empty");
+
+        // Ensure one per address
+        require(vestedAmount[_beneficiary] == 0, "Schedule already in flight");
+
+        vestedAmount[_beneficiary] = _amount;
+
+        emit ScheduleCreated(_beneficiary);
+
+        return true;
     }
 
     function claim() external iscrowdsaleOver {
@@ -229,13 +321,71 @@ contract Crowdsale  {
         token.transfer(msg.sender, claimable[msg.sender]);
         claimable[msg.sender] = 0;
     }
+    
+    /**
+     * @notice Draws down any vested tokens due
+     * @dev Must be called directly by the beneficiary assigned the tokens in the schedule
+     */
+    function drawDown() nonReentrant external returns (bool) {
+        return _drawDown(msg.sender);
+    }
+    
+    function _drawDown(address _beneficiary) internal returns (bool) {
+        require(vestedAmount[_beneficiary] > 0, "There is no schedule currently in flight");
 
-    function getEthBalance() public view returns (uint256) {
-        return address(this).balance;
+        uint256 amount = _availableDrawDownAmount(_beneficiary);
+        require(amount > 0, "No allowance left to withdraw");
+
+        // Update last drawn to now
+        lastDrawnAt[_beneficiary] = _getNow();
+
+        // Increase total drawn amount
+        totalDrawn[_beneficiary] = totalDrawn[_beneficiary].add(amount);
+
+        // Safety measure - this should never trigger
+        require(
+            totalDrawn[_beneficiary] <= vestedAmount[_beneficiary],
+            "Safety Mechanism - Drawn exceeded Amount Vested"
+        );
+
+        // Issue tokens to beneficiary
+        require(token.transfer(_beneficiary, amount), "Unable to transfer tokens");
+
+        emit DrawDown(_beneficiary, amount);
+
+        return true;
     }
 
-    function adminTransferEthFund() external onlyOwner {
-        msg.sender.transfer(address(this).balance);
+    function _getNow() internal view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function _availableDrawDownAmount(address _beneficiary) internal view returns (uint256 _amount) {
+
+        // Cliff Period
+        if (_getNow() <= start.add(cliffDuration)) {
+            // the cliff period has not ended, no tokens to draw down
+            return 0;
+        }
+
+        // Schedule complete
+        if (_getNow() > end) {
+            return vestedAmount[_beneficiary].sub(totalDrawn[_beneficiary]);
+        }
+
+        // Schedule is active
+
+        // Work out when the last invocation was
+        uint256 timeLastDrawnOrStart = lastDrawnAt[_beneficiary] == 0 ? start : lastDrawnAt[_beneficiary];
+
+        // Find out how much time has past since last invocation
+        uint256 timePassedSinceLastInvocation = _getNow().sub(timeLastDrawnOrStart);
+
+        // Work out how many due tokens - time passed * rate per second
+        uint256 drawDownRate = vestedAmount[_beneficiary].div(end.sub(start));
+        uint256 amount = timePassedSinceLastInvocation.mul(drawDownRate);
+
+        return amount;
     }
 
     function getContractTokenBalance(IERC20 _token)
@@ -257,6 +407,7 @@ contract Crowdsale  {
 
         _token.transfer(msg.sender, value);
     }
+    
 
     function doTransferIn(
         address tokenAddress,
